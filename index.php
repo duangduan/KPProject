5b71bca811520fff833d4ab0bb3d8d9cd3f88f88
kp-team <3nitro2-luene@hotmail.co.th>
2014-05-17 11:30:01 +0700

    added new index

--- index.php
+++ index.php
@@ -46,43 +46,7 @@ include("connectDB/connect.php");
         <h2>รายชื่อผู้บรรยาย</h2>
       </section>
 	  
-	  <?php
-	  
-	  $slqSelectDub = "SELECT * FROM dub";
-	  $sqlQueryDub = mysql_query($slqSelectDub);
-	  
-	  while($slqSelectDub = mysql_fetch_array($sqlQueryDub)){
-	  
-	  		//$img_source_sr = "images/noimage02.jpg";
-			$full_name	= $$slqSelectDub[1];
-	  	
-	
-	  
-	  ?>
-	  
-	  
-      <div class="row">
-        <div class="12u">
-          <div class="row">
-            <div class="3u">
-              <section>
-                <div>
-                  <div class="image-style1"><a href="#"><img src="images/pics06.jpg" alt=""></a></div>
-                  <div><a href="#"><img src="css/images/img02.png" width="262" height="30" alt=""></a></div>
-                </div>
-                <p> <? echo $full_name;?></p>
-                <p class="button"><a href="#">Read Full Details</a></p>
-              </section>
-            </div>
-            
-          </div>
-        </div>
-      </div>
-    </div>
-	
-	<?
-	}
-	?>
+	 
 	<div id="feature-content">
       <div class="row">
         <div class="12u">
